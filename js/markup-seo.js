21e160f6b995a7ecb22221568cca94504ef39881
O.Yann André <115635958+Perito009@users.noreply.github.com>
2025-07-25 18:06:32 +0000

    ajout lien insta et autre

---
+++ js/markup-seo.js
@@ -0,0 +1,36 @@
+type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "CleaningService",
+        "name": "SFSC Propreté",
+        "image": "https://www.sfscproprete.fr/images/SFSC_Logo-1.jpg",
+        "description": "Services de nettoyage professionnel à Lyon : bureaux, commerces et particuliers",
+        "areaServed": {
+            "@type": "City",
+            "name": "Lyon"
+        },
+        "address": {
+            "@type": "PostalAddress",
+            "streetAddress": "Rue des Charmettes",
+            "addressLocality": "Lyon",
+            "postalCode": "69100",
+            "addressCountry": "FR"
+        },
+        "aggregateRating": {
+            "@type": "AggregateRating",
+            "ratingValue": "4.8",
+            "reviewCount": "150"
+        },
+        "priceRange": "€€",
+        "telephone": "06 61 85 39 39",
+        "openingHours": ["Mo-Sa 08:00-19:00"],
+        "serviceArea": {
+            "@type": "GeoCircle",
+            "geoMidpoint": {
+                "@type": "GeoCoordinates",
+                "latitude": "45.7671397",
+                "longitude": "4.8527611"
+            },
+            "geoRadius": "30000"
+        }
+    }
